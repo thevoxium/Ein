987341b5d9ba3feffa339fcc469f12d1a6ed905f
thevoxium <thevoxium@gmail.com>
2026-02-20 16:39:42 +0530

    helper functions

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "lexer.h"
 
 Parser *init_parser(Lexer *lexer) {
   Parser *p = (Parser *)malloc(sizeof(Parser));
@@ -26,4 +27,45 @@ void free_parser(Parser *p) {
 
   free(p->tokens);
   free(p);
+}
+
+Token peek(Parser *p) { return p->tokens[p->current]; }
+
+Token previous(Parser *p) {
+  assert(p->current > 0);
+  return p->tokens[p->current - 1];
+}
+
+bool is_at_end(Parser *p) { return (p->current >= p->token_count); }
+
+bool check(Parser *p, TokenType tokenType) {
+  if (is_at_end(p))
+    return false;
+  return (p->tokens[p->current].tokenType == tokenType);
+}
+
+Token advance(Parser *p) {
+  Token t = p->tokens[p->current];
+  p->current++;
+  return t;
+}
+
+bool match(Parser *p, TokenType *types, int count) {
+  for (int i = 0; i < count; i++) {
+    if (check(p, types[i])) {
+      advance(p);
+      return true;
+    }
+  }
+  return false;
+}
+
+Token expect(Parser *p, TokenType tokenType) {
+  if (check(p, tokenType)) {
+    return advance(p);
+  }
+
+  fprintf(stderr, "Parse error at line %d: expected token type %d, got '%s'\n",
+          peek(p).line, tokenType, peek(p).literal);
+  exit(1);
 }
--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "assert.h"
 #include "ast.h"
 #include "lexer.h"
 
@@ -12,5 +13,13 @@ typedef struct Parser {
 
 Parser *init_parser(Lexer *lexer);
 void free_parser(Parser *p);
+
+Token peek(Parser *p);
+Token previous(Parser *p);
+bool is_at_end(Parser *p);
+bool check(Parser *p, TokenType tokenType);
+Token advance(Parser *p);
+bool match(Parser *p, TokenType *types, int count);
+Token expect(Parser *p, TokenType tokenType);
 
 #endif // !PARSER_H
